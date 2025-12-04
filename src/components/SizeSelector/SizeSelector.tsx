e6287264e9233d4d24f0c6a6ad62332af9e7fa28
Travis Adams <brokenCD@live.com>
2025-12-04 13:56:46 +0700

    make state call consistent

--- src/components/SizeSelector/SizeSelector.tsx
+++ src/components/SizeSelector/SizeSelector.tsx
@@ -5,8 +5,8 @@ import { FormSelect } from '../FormSelect/FormSelect';
 
 export function SizeSelector() {
     const pizzaShape = usePizzaStore(state => state.shape);
-    const pizzaSize = usePizzaStore(s => s.size);
-    const setPizzaSize = usePizzaStore(s => s.setSize);
+    const pizzaSize = usePizzaStore(state => state.size);
+    const setPizzaSize = usePizzaStore(state => state.setSize);
 
     const options = sizeOptionsForShape(pizzaShape);
     
