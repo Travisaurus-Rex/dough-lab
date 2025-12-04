f2218e9ac48bbe1f3c32f66f07c253d295d3e8fb
Travis Adams <brokenCD@live.com>
2025-12-04 11:30:32 +0700

    update Shape Selector component to use new select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ShapeSelector } from './components/ShapeSelector/ShapeSelector'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-4xl mx-auto my-6 p-4 border border-black rounded-sm">
+        <h1 className="text-5xl mb-6">DoughLab</h1>
+        <div className="grid grid-cols-2">
+          <ShapeSelector />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- src/components/ShapeSelector/ShapeSelector.tsx
+++ src/components/ShapeSelector/ShapeSelector.tsx
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 import { PizzaShape, type PizzaShape as PizzaShapeType } from '../../models/PizzaShape';
+import { FormSelect } from '../FormSelect/FormSelect';
 
 export function ShapeSelector() {
     const [shape, setShape] = useState<PizzaShapeType>('none');
 
     return (
-        <>
-        <label>Pan Shape:</label>
-        <select value={shape} onChange={e => setShape(e.target.value as PizzaShapeType)}>
-            {PizzaShape.map((value: PizzaShape) => (
-                <option key={value} value={value}>
-                    {value}
-                </option>
-            ))}
-        </select>
-      </>
+        <div>
+            <FormSelect 
+                label="Pizza Shape"
+                options={PizzaShape}
+                value={shape}
+                onChange={(val) => setShape(val)} />
+        </div>
     )
 }
