629d3dee932dcf1c603b685a24097769587e1f67
Travis Adams <brokenCD@live.com>
2025-12-04 13:28:56 +0700

    update sizes

--- src/models/PizzaSize.ts
+++ src/models/PizzaSize.ts
@@ -1,17 +1,17 @@
-export const CircleSizes = ['10"', '12"', '14"'] as const;
+export const CircleSizes = ['8"', '10"', '12"', '14"'] as const;
 export type CircleSize = (typeof CircleSizes)[number];
 
-export const RectangleSizes = ['6x6', '9x9', '12x12'] as const;
+export const RectangleSizes = ['6x9', '12x16', '13x18'] as const;
 export type RectangleSize = (typeof RectangleSizes)[number];
 
-export const SquareSizes = ['8x8', '10x10'] as const;
+export const SquareSizes = ['8x8', '9x9', '12x12', '16x16'] as const;
 export type SquareSize = (typeof SquareSizes)[number];
 
 export type PizzaSize =
   | CircleSize
   | RectangleSize
   | SquareSize;
-  
+
 export type PizzaSizePlaceholder =
   | 'NONE'
   | 'SELECT A PIZZA SHAPE';
