b327dc0c0e57d2840d68e8151884b10328a0f66b
Travis Adams <brokenCD@live.com>
2025-12-04 13:26:50 +0700

    add a couple extra styles

--- src/models/PizzaStyle.ts
+++ src/models/PizzaStyle.ts
@@ -1,2 +1,2 @@
-export const PizzaStyle = ['NONE', 'NYC', 'CHICAGO', 'DETROIT', 'MARGHERITA', 'NAPOLETANA'];
+export const PizzaStyle = ['NONE', 'NYC', 'CHICAGO', 'MARGHERITA', 'NAPOLETANA', 'DETROIT', 'SICILIAN','GRANDMA'];
 export type PizzaStyle = (typeof PizzaStyle)[number];
