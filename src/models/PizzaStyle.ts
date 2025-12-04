6ee21bc5f7e55a70b0457636d04c0ea2a9b30510
Travis Adams <brokenCD@live.com>
2025-12-04 15:24:23 +0700

    create real pizza style without uncalculatable data

--- src/models/PizzaStyle.ts
+++ src/models/PizzaStyle.ts
@@ -1,2 +1,3 @@
 export const PizzaStyle = ['NONE', 'NYC', 'CHICAGO', 'MARGHERITA', 'NAPOLETANA', 'DETROIT', 'SICILIAN','GRANDMA'];
 export type PizzaStyle = (typeof PizzaStyle)[number];
+export type RealPizzaStyle = Exclude<PizzaStyle, 'NONE'>;
